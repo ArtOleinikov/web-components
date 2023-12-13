64aab0d0b91e6dc23a752b0c14e097b752ffbcc0
Artem Oleinikov <artem.oleinikov.web@gmail.com>
2023-12-13 11:49:53 +0200

    - update format

--- src/js-btn.js
+++ src/js-btn.js
@@ -1,56 +1,57 @@
 class JsBtn extends HTMLElement {
-	constructor() {
-		super();
-		this.attachShadow({ mode: 'open' });
-		this.exampleValue = this.getAttribute('exampleValue');
-	}
-	
-	static get observedAttributes() {
-		return ["exampleValue"];
-	}
-	
-	connectedCallback() {
-		this.render();
-		this.shadowRoot.querySelector('button').addEventListener('click', () => this.completedEvent());
-	}
-	
-	disconnectedCallback() {
-		this.shadowRoot.querySelector('button').removeEventListener('click', () => this.completedEvent());
-	}
-	
-	get exampleValue() {
-		return this.getAttribute("exampleValue");
-	}
-	set exampleValue(value) {
-		this.setAttribute("exampleValue", value);
-		this.render();
-	}
-	
-	
-	attributeChangedCallback(name, oldValue, newValue) {
-		if (name === 'exampleValue' && oldValue !== newValue) {
-			this.exampleValue = newValue;
-			this.render();
-			
-		}
-	}
-	
-	completedEvent() {
-		const customEvent = new CustomEvent('example-event', {
-			bubbles: true,
-			composed: true,
-			detail: this.exampleValue
-		});
-		this.dispatchEvent(customEvent);
-	}
-	
-	render() {
-		this.shadowRoot.innerHTML = `
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.exampleValue = this.getAttribute('exampleValue');
+  }
+  
+  static get observedAttributes() {
+    return ["exampleValue"];
+  }
+  
+  connectedCallback() {
+    this.render();
+    this.shadowRoot.querySelector('button').addEventListener('click', () => this.completedEvent());
+  }
+  
+  disconnectedCallback() {
+    this.shadowRoot.querySelector('button').removeEventListener('click', () => this.completedEvent());
+  }
+  
+  get exampleValue() {
+    return this.getAttribute("exampleValue");
+  }
+  
+  set exampleValue(value) {
+    this.setAttribute("exampleValue", value);
+    this.render();
+  }
+  
+  
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'exampleValue' && oldValue !== newValue) {
+      this.exampleValue = newValue;
+      this.render();
+      
+    }
+  }
+  
+  completedEvent() {
+    const customEvent = new CustomEvent('example-event', {
+      bubbles: true,
+      composed: true,
+      detail: this.exampleValue
+    });
+    this.dispatchEvent(customEvent);
+  }
+  
+  render() {
+    this.shadowRoot.innerHTML = `
       <button type="${this.exampleValue}" onclick="this.getRootNode().host.completedEvent()">
         <slot name="title">Default Title</slot>, type: ${this.exampleValue}
       </button>
     `;
-	}
+  }
 }
 
 window.customElements.define('js-btn', JsBtn);
